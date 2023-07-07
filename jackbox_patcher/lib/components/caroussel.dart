72f4bc4d191cc395d6cdd0439774341ef608def4
AlexisL61 <alexisleray61@outlook.com>
2023-07-07 22:44:33 +0200

    #223 - Fix full-screen

--- jackbox_patcher/lib/components/caroussel.dart
+++ jackbox_patcher/lib/components/caroussel.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:jackbox_patcher/components/blurhashimage.dart';
@@ -54,9 +55,15 @@ class _AssetCarousselWidgetState extends State<AssetCarousselWidget> {
   void checkingIfHasVideo() {
     for (var i = 0; i < widget.images.length; i++) {
       if (isAVideo(widget.images[i])) {
-        hasVideo = true;
-        controller = VideoController(player);
-        break;
+        if (Platform.isLinux) {
+          hasVideo = false;
+          widget.images.removeAt(i);
+          i--;
+        } else {
+          hasVideo = true;
+          controller = VideoController(player);
+          break;
+        }
       }
     }
   }
@@ -104,7 +111,7 @@ class _AssetCarousselWidgetState extends State<AssetCarousselWidget> {
       } else {
         player.stop();
       }
-    }else{
+    } else {
       player.stop();
     }
   }
@@ -112,130 +119,146 @@ class _AssetCarousselWidgetState extends State<AssetCarousselWidget> {
   @override
   Widget build(BuildContext context) {
     return MaterialDesktopVideoControlsTheme(
-      normal: const MaterialDesktopVideoControlsThemeData(
-        bottomButtonBar: const [
-          MaterialDesktopSkipPreviousButton(), MaterialDesktopPlayOrPauseButton(), MaterialDesktopSkipNextButton(), MaterialDesktopVolumeButton(), MaterialDesktopPositionIndicator(), Spacer()
-        ],
-      ),
-      fullscreen: const MaterialDesktopVideoControlsThemeData(),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(8.0),
-        child: SizedBox(
-            width: double.maxFinite,
-            child: AspectRatio(
-                aspectRatio: 1.778,
-                child: GestureDetector(
-                    child: MouseRegion(
-                        onEnter: (a) => setState(() {
-                              moveButtonVisible = true;
-                            }),
-                        onExit: (a) => setState(() {
-                              moveButtonVisible = false;
-                            }),
-                        child: Stack(children: [
-                          !isAVideo(widget.images[imageIndex])
-                              ? BlurHashImage(
-                                  url: widget.images[imageIndex],
-                                  fit: BoxFit.fitWidth,
-                                )
-                              : (isVideoLoaded
-                                  ? Video(
-                                      key: Key(widget.images[0]),
-                                      controller: controller,
-                                      controls: (VideoState? state) {
-                                        if (state != null) {
-                                          return AdaptiveVideoControls(state);
-                                        } else {
-                                          return SizedBox.shrink();
-                                        }
-                                      })
-                                  : Container(
-                                      color: Colors.black,
-                                      child: Center(
-                                        child: ProgressRing(
-                                          activeColor: Colors.white,
-                                        ),
-                                      ),
-                                    )),
-                          tweenAnimationBuilder = TweenAnimationBuilder<double>(
-                              onEnd: () {
-                                if (!moveButtonVisible &&
-                                    changingImage == false) {
-                                  setState(() {
-                                    imageIndex =
-                                        (imageIndex + 1) % widget.images.length;
-                                    changingImage = true;
-                                  });
-                                  startVideo();
-                                  if (!isAVideo(widget.images[imageIndex])) {
-                                    Future.delayed(
-                                        const Duration(milliseconds: 1100),
-                                        () => setState(() {
-                                              changingImage = false;
-                                            }));
-                                  }
-                                }
-                              },
-                              tween: Tween<double>(
-                                begin:
-                                    moveButtonVisible || changingImage ? 1 : 0,
-                                end: moveButtonVisible || changingImage ? 0 : 1,
-                              ),
-                              curve: Curves.easeOut,
-                              duration: moveButtonVisible || changingImage
-                                  ? const Duration(milliseconds: 1000)
-                                  : const Duration(seconds: 6),
-                              builder: (BuildContext context, double widthTween,
-                                  Widget? child) {
-                                return FractionallySizedBox(
-                                    alignment: Alignment.bottomCenter,
-                                    widthFactor: changingImage ? 1 : widthTween,
-                                    heightFactor: 1,
-                                    child: Column(
+        normal: const MaterialDesktopVideoControlsThemeData(
+          bottomButtonBar: const [
+            MaterialDesktopSkipPreviousButton(),
+            MaterialDesktopPlayOrPauseButton(),
+            MaterialDesktopSkipNextButton(),
+            MaterialDesktopVolumeButton(),
+            MaterialDesktopPositionIndicator(),
+            Spacer()
+          ],
+        ),
+        fullscreen: const MaterialDesktopVideoControlsThemeData(
+          toggleFullscreenOnDoublePress: false
+        ),
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: SizedBox(
+                width: double.maxFinite,
+                child: AspectRatio(
+                    aspectRatio: 1.778,
+                    child: GestureDetector(
+                        child: MouseRegion(
+                            onEnter: (a) => setState(() {
+                                  moveButtonVisible = true;
+                                }),
+                            onExit: (a) => setState(() {
+                                  moveButtonVisible = false;
+                                }),
+                            child: Stack(children: [
+                              !isAVideo(widget.images[imageIndex])
+                                  ? BlurHashImage(
+                                      url: widget.images[imageIndex],
+                                      fit: BoxFit.fitWidth,
+                                    )
+                                  : (isVideoLoaded
+                                      ? Video(
+                                          key: Key(widget.images[0]),
+                                          controller: controller,
+                                          controls: (VideoState? state) {
+                                            if (state != null) {
+                                              return AdaptiveVideoControls(
+                                                  state);
+                                            } else {
+                                              return SizedBox.shrink();
+                                            }
+                                          })
+                                      : Container(
+                                          color: Colors.black,
+                                          child: Center(
+                                            child: ProgressRing(
+                                              activeColor: Colors.white,
+                                            ),
+                                          ),
+                                        )),
+                              tweenAnimationBuilder = TweenAnimationBuilder<
+                                      double>(
+                                  onEnd: () {
+                                    if (!moveButtonVisible &&
+                                        changingImage == false) {
+                                      setState(() {
+                                        imageIndex = (imageIndex + 1) %
+                                            widget.images.length;
+                                        changingImage = true;
+                                      });
+                                      startVideo();
+                                      if (!isAVideo(
+                                          widget.images[imageIndex])) {
+                                        Future.delayed(
+                                            const Duration(milliseconds: 1100),
+                                            () => setState(() {
+                                                  changingImage = false;
+                                                }));
+                                      }
+                                    }
+                                  },
+                                  tween: Tween<double>(
+                                    begin: moveButtonVisible || changingImage
+                                        ? 1
+                                        : 0,
+                                    end: moveButtonVisible || changingImage
+                                        ? 0
+                                        : 1,
+                                  ),
+                                  curve: Curves.easeOut,
+                                  duration: moveButtonVisible || changingImage
+                                      ? const Duration(milliseconds: 1000)
+                                      : const Duration(seconds: 6),
+                                  builder: (BuildContext context,
+                                      double widthTween, Widget? child) {
+                                    return FractionallySizedBox(
+                                        alignment: Alignment.bottomCenter,
+                                        widthFactor:
+                                            changingImage ? 1 : widthTween,
+                                        heightFactor: 1,
+                                        child: Column(
+                                          children: [
+                                            Spacer(),
+                                            Container(
+                                                height: 3,
+                                                color: Colors.blue.withOpacity(
+                                                    changingImage
+                                                        ? widthTween / 2
+                                                        : 0.5)),
+                                          ],
+                                        ));
+                                  }),
+                              moveButtonVisible
+                                  ? Center(
+                                      child: Row(
                                       children: [
-                                        Spacer(),
-                                        Container(
-                                            height: 3,
-                                            color: Colors.blue.withOpacity(
-                                                changingImage
-                                                    ? widthTween / 2
-                                                    : 0.5)),
+                                        const SizedBox(
+                                          width: 10,
+                                        ),
+                                        GestureDetector(
+                                            onTap: () => setState(() {
+                                                  imageIndex = (imageIndex -
+                                                          1) %
+                                                      (widget.images.length);
+                                                  changingImage = false;
+                                                  startVideo();
+                                                }),
+                                            child: const Icon(FluentIcons
+                                                .chevron_left_small)),
+                                        const Expanded(child: SizedBox()),
+                                        GestureDetector(
+                                            onTap: () => setState(() {
+                                                  imageIndex = (imageIndex +
+                                                          1) %
+                                                      (widget.images.length);
+                                                  changingImage = false;
+                                                  startVideo();
+                                                }),
+                                            child: const Icon(FluentIcons
+                                                .chevron_right_small)),
+                                        const SizedBox(
+                                          width: 10,
+                                        ),
                                       ],
-                                    ));
-                              }),
-                          moveButtonVisible
-                              ? Center(
-                                  child: Row(
-                                  children: [
-                                    const SizedBox(
-                                      width: 10,
-                                    ),
-                                    GestureDetector(
-                                        onTap: () => setState(() {
-                                              imageIndex = (imageIndex - 1) %
-                                                  (widget.images.length);
-                                              changingImage = false;
-                                              startVideo();
-                                            }),
-                                        child: const Icon(
-                                            FluentIcons.chevron_left_small)),
-                                    const Expanded(child: SizedBox()),
-                                    GestureDetector(
-                                        onTap: () => setState(() {
-                                              imageIndex = (imageIndex + 1) %
-                                                  (widget.images.length);
-                                              changingImage = false;
-                                              startVideo();
-                                            }),
-                                        child: const Icon(
-                                            FluentIcons.chevron_right_small)),
-                                    const SizedBox(
-                                      width: 10,
-                                    ),
-                                  ],
-                                ))
-                              : Container()
-                        ])))))));
+                                    ))
+                                  : Container()
+                            ])))))));
   }
 
   bool isAVideo(String url) => url.contains(".mp4");
