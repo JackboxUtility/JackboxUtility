703228d52f2da087022029dc7c2552ad5afce032
ACertainCoder <47316898+ACertainCoder@users.noreply.github.com>
2024-10-04 18:26:09 +0200

    Download improvements (#424)
    * chore: Update gitignore to exclude cmake files
    
    Excludes some files generated by cmake and the build process
    
    * docs: Add build requirements
    
    Add requirements to contribution section in the readme
    
    * chore: Update VSCode settings and extensions
    
    Update settings to use relative paths and recommendation to include the conventional commits extension.
    
    * fix: Avoid caroussel error if disposed
    
    Fixes a rendering error when a caroussel change was processed even though the component was disposed
    
    * feat(internal): Add custom DioMixin
    
    Add custom Dio implementation that is based on DioForNative
    
    * feat: Improve download handling
    
    Improvements to UI and error handling
    
    * chore: Revise code
    
    Use HttpStatus and rename file instead of copying and deleting the original
    
    ---------
    
    Co-authored-by: ACertainCoder <ACertainCoder@users.noreply.github.com>

--- jackbox_patcher/lib/model/user_model/interface/installable_patch.dart
+++ jackbox_patcher/lib/model/user_model/interface/installable_patch.dart
@@ -1,8 +1,22 @@
 import 'package:dio/dio.dart';
+import 'package:jackbox_patcher/model/user_model/user_jackbox_game_patch.dart';
 import 'package:jackbox_patcher/model/user_model/user_jackbox_pack.dart';
+import 'package:jackbox_patcher/model/user_model/user_jackbox_pack_patch.dart';
 
 abstract class InstallablePatch {
-  Future<void> downloadPatch(String patchUri,
-      void Function(String, String, double) callback, CancelToken cancelToken);
+  Future<void> downloadPatch(
+      String patchUri,
+      void Function(String, String, double?) callback,
+      CancelToken cancelToken,
+      bool resume);
   UserJackboxPack getPack();
+  String getName() {
+    if (this is UserJackboxGamePatch) {
+      return (this as UserJackboxGamePatch).getGame().game.name;
+    } else if (this is UserJackboxPackPatch) {
+      return getPack().pack.name;
+    } else {
+      return getPack().pack.name;
+    }
+  }
 }
--- jackbox_patcher/lib/services/downloader/downloader_service.dart
+++ jackbox_patcher/lib/services/downloader/downloader_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:jackbox_patcher/model/user_model/interface/installable_patch.dart';
 import 'package:jackbox_patcher/services/logger/logger.dart';
 
 import '../api_utility/api_service.dart';
@@ -10,39 +11,59 @@ import '../translations/translations_helper.dart';
 class DownloaderService {
   static bool isDownloading = false;
 
+  /// Fetches the patch file size
+  static Future<int> getPatchSize(patchUrl) {
+    return APIService().fetchFileSize(patchUrl);
+  }
+
   /// Downloads a patch from [patchUrl] and extracts it to [uri]
-  static Future<void> downloadPatch(String uri, String patchUrl,
-      void Function(String, String, double) callback, CancelToken cancelToken) async {
-    JULogger().i("[DownloaderService] Downloading patch from $patchUrl to $uri");
+  static Future<void> downloadPatch(
+      InstallablePatch patch,
+      String uri,
+      String patchUrl,
+      void Function(String, String, double?) callback,
+      CancelToken cancelToken,
+      bool resume) async {
+    JULogger()
+        .i("[DownloaderService] Downloading patch from $patchUrl to $uri");
     isDownloading = true;
     String filePath = "";
     try {
       callback(TranslationsHelper().appLocalizations!.downloading,
-          TranslationsHelper().appLocalizations!.starting, 0);
-      filePath = await APIService().downloadPatch(patchUrl,
+          TranslationsHelper().appLocalizations!.starting, null);
+      filePath = await APIService().downloadPatch(patch, patchUrl,
           (double progress, double max) {
         callback(
             TranslationsHelper().appLocalizations!.downloading,
-            "${progress ~/ 1000000} MB / ${max ~/ 1000000} MB",
+            "${(progress / (1000 * 1000)).toStringAsFixed(2)} MB / ${(max / (1000 * 1000)).toStringAsFixed(2)} MB",
             (progress / max) * 75);
-      }, cancelToken);
+      }, cancelToken, resume);
+    } on DioError catch (e) {
+      if (e.type == DioErrorType.cancel) {
+        JULogger().i("[DownloaderService] Patch download cancelled");
+      } else {
+        JULogger().e("[DownloaderService] Patch download failed: $e");
+      }
+      isDownloading = false;
+      rethrow;
     } catch (e) {
-      JULogger().e("[DownloaderService] $e");
+      JULogger().e("[DownloaderService] Patch download failed: $e");
       isDownloading = false;
       rethrow;
     }
     try {
-      JULogger().i("[DownloaderService] Extracting patch from $filePath to $uri");
+      JULogger()
+          .i("[DownloaderService] Extracting patch from $filePath to $uri");
       callback(TranslationsHelper().appLocalizations!.extracting, "", 75);
 
       await extractFileToDisk(filePath, uri, callback);
       callback(TranslationsHelper().appLocalizations!.finalizing, "", 100);
     } catch (e) {
-      JULogger().e("[DownloaderService] $e");
-      isDownloading = false;
+      JULogger().e("[DownloaderService] Patch extraction failed: $e");
       rethrow;
+    } finally {
+      isDownloading = false;
     }
-    isDownloading = false;
     //File(filePath).deleteSync(recursive: true);
   }
 
@@ -84,13 +105,12 @@ class DownloaderService {
     Process process =
         await Process.start("tar", ["-xf", '$filePath', "-C", '$uri', "-v"]);
     int currentFiles = 0;
-    process.stdout.listen((data) {});
     process.stderr.listen((data) {
       try {
         currentFiles += utf8.decode(data).split("\n").length - 1;
         callback(TranslationsHelper().appLocalizations!.extracting,
             "$currentFiles/$files", 75 + ((currentFiles / files) * 25));
-      }catch(e){
+      } catch (e) {
         JULogger().e("[DownloaderService] $e");
       }
     });
